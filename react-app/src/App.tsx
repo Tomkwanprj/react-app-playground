bed79264b0c29c7ede150aa14762e0826364a83d
Tomkwanprj <tomkwanyingkin@gmail.com>
2023-07-25 23:31:08 +0800

    feat: implement useState and share the state

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -5,6 +5,9 @@ import ListGroup from "./components/ListGroup";
 import Like from "./components/Like";
 import "./App.css";
 import { BsFillCalendarCheckFill } from "react-icons/bs";
+import produce from "immer";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 /* 
   Props                        State
@@ -50,7 +53,37 @@ function App() {
   });
 
   const handlePersonClick = () => {
-    setPerson({ ...person, address: { ...person.address, zipCode: 123123 } });
+    setPerson({ ...person, address: { ...person.address, zipCode: 23123 } });
+  };
+
+  const [tags, setTags] = useState(["happy", "cheerful"]);
+
+  const handleTagsClick = () => {
+    setTags([...tags]);
+  };
+
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
+
+  const handleBugsClick = () => {
+    // setBugs([...bugs, { id: 3, title: "Bug 3", fixed: false }]);
+    // setBugs(bugs.map((e) => (e.id == 1 ? { ...e, fixed: true } : e)));
+    // use immer
+
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
+  };
+
+  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
+
+  const handleClear = () => {
+    setCartItems([]);
   };
 
   return (
@@ -71,12 +104,21 @@ function App() {
 
       <Like onLikeClicked={handleLikeItem}></Like>
 
-      {person.address.zipCode}
-      <Button
-        children="Hello World"
-        color="primary"
-        onButtonClicked={handlePersonClick}
-      ></Button>
+      <div>
+        {bugs.map((bug) => (
+          <p key={bug.id}>{bug.title + ": " + bug.fixed}</p>
+        ))}
+        <Button
+          children="Hello World"
+          color="primary"
+          onButtonClicked={handleBugsClick}
+        ></Button>
+      </div>
+
+      <div>
+        <NavBar cartItemsCount={cartItems.length}></NavBar>
+        <Cart cartItems={cartItems} onClear={handleClear}></Cart>
+      </div>
     </>
   );
 }
