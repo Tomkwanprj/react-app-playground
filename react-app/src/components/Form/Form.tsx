a73f25b56cbfdad67e3f546c968b6231d639657e
Tomkwanprj <tomkwanyingkin@gmail.com>
2023-07-30 10:44:27 +0800

    feat: using react state hook to manage the form

--- react-app/src/components/Form/Form.tsx
+++ react-app/src/components/Form/Form.tsx
@@ -1,17 +1,28 @@
-import { useRef, FormEvent } from "react";
+import { useState, useRef, FormEvent } from "react";
 
 const Form = () => {
   // Current property of ref object references a DOM node.
   // Initially, when we create a ref object, we dont have access to the DOM node
   // because the DOM is created after react renders our componenets
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 };
+
+  //  const nameRef = useRef<HTMLInputElement>(null);
+  //  const ageRef = useRef<HTMLInputElement>(null);
+  //  const person = { name: "", age: 0 };
+
+  // Using react state hook will render our compoenet every time when user change the input field,
+  // it might cause performance issue.
+  // Also, input fields have their own state and we have react state called person. It is possible that
+  // these sources get out of sync. We should maintain the single source of truth and singleton.
+
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current !== null) person.name = nameRef.current.value;
-    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+    // if (nameRef.current !== null) person.name = nameRef.current.value;
+    // if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
     console.log(person);
   };
 
@@ -23,7 +34,15 @@ const Form = () => {
           <label htmlFor="name" className="form-label">
             Name
           </label>
-          <input ref={nameRef} id="name" type="text" className="form-control" />
+          <input
+            value={person.name}
+            onChange={(event) =>
+              setPerson({ ...person, name: event.target.value })
+            }
+            id="name"
+            type="text"
+            className="form-control"
+          />
         </div>
 
         {/* div.mb-3>label.form-label+input[type=number].form-control */}
@@ -31,7 +50,15 @@ const Form = () => {
           <label htmlFor="age" className="form-label">
             Age
           </label>
-          <input ref={ageRef} id="age" type="number" className="form-control" />
+          <input
+            value={person.age}
+            onChange={(event) =>
+              setPerson({ ...person, age: parseInt(event.target.value) })
+            }
+            id="age"
+            type="number"
+            className="form-control"
+          />
         </div>
         <br />
         <button className="btn btn-primary" type="submit">
